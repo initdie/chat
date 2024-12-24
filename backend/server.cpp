f6835cb99d7091687c782189df968080b9e50551
Dmitry <initdie@gmail.com>
2024-12-24 20:16:10 +0200

    Add coments to code

--- backend/server.cpp
+++ backend/server.cpp
@@ -6,23 +6,25 @@
 
 using boost::asio::ip::tcp;
 
-// Message structure to handle chat messages
+// Message structure for chat messages
 struct ChatMessage {
-    static constexpr size_t max_length = 1024;
-    char data[max_length];
-    size_t length;
+    static constexpr size_t max_length = 1024;  // Maximum message size
+    char data[max_length];                      // Buffer for message data
+    size_t length;                             // Actual message length
 };
 
+// Handles individual client connections
 class ChatSession : public std::enable_shared_from_this<ChatSession> {
 public:
     ChatSession(tcp::socket socket)
-        : socket_(std::move(socket)) {}
+        : socket_(std::move(socket)) {}    // Takes ownership of the socket
 
     void start() {
-        do_read();
+        do_read();  // Start reading from client
     }
 
 private:
+    // Asynchronously read from client
     void do_read() {
         auto self(shared_from_this());
         socket_.async_read_some(
@@ -30,12 +32,14 @@ private:
             [this, self](boost::system::error_code ec, std::size_t length) {
                 if (!ec) {
                     read_msg_.length = length;
+                    // Print received message
                     std::cout << "Received: " << std::string(read_msg_.data, length) << std::endl;
-                    do_write();
+                    do_write();  // Echo message back
                 }
             });
     }
 
+    // Asynchronously write back to client
     void do_write() {
         auto self(shared_from_this());
         boost::asio::async_write(
@@ -43,32 +47,35 @@ private:
             boost::asio::buffer(read_msg_.data, read_msg_.length),
             [this, self](boost::system::error_code ec, std::size_t /*length*/) {
                 if (!ec) {
-                    do_read();
+                    do_read();  // Continue reading after write completes
                 }
             });
     }
 
-    tcp::socket socket_;
-    ChatMessage read_msg_;
+    tcp::socket socket_;        // Socket for client connection
+    ChatMessage read_msg_;      // Buffer for incoming messages
 };
 
+// Main server class
 class ChatServer {
 public:
     ChatServer(boost::asio::io_context& io_context, short port)
         : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)) {
-        do_accept();
+        do_accept();  // Start accepting connections
     }
 
 private:
+    // Asynchronously accept new connections
     void do_accept() {
         acceptor_.async_accept(
             [this](boost::system::error_code ec, tcp::socket socket) {
                 if (!ec) {
+                    // Create new session for each client
                     std::make_shared<ChatSession>(std::move(socket))->start();
                 }
-                do_accept();
+                do_accept();  // Continue accepting new connections
             });
     }
 
-    tcp::acceptor acceptor_;
+    tcp::acceptor acceptor_;  // Accepts incoming connections
 };
